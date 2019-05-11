e597c0ba329a0d0c034e80f9b17c02a2e755ca77
祐太郎萩原 <s21161062@gmail.com>
2019-05-11 19:53:14 +0900

    Initial Commit

---
+++ ElectronicMedicalRecord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElectronicMedicalRecord
+//
+//  Created by 萩原祐太郎 on 2019/05/11.
+//  Copyright © 2019 Yutaro_Hagiwara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
